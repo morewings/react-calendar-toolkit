e43f0ba07cb25505205c6ed26e396a23180caf10
Dima Vyshniakov <morewings@gmail.com>
2020-02-01 21:40:54 +0100

    WIP

--- src/components/DayGrid/DayGrid.js
+++ src/components/DayGrid/DayGrid.js
@@ -1,13 +1,26 @@
 import React, {Fragment} from 'react';
-import WeekDays from './WeekDays';
-import Days from './Days';
+import {getWeekDayNames, getMonthDays} from 'utils/dateUtils';
+import Weekday from 'components/visual/WeekDay';
+import Day from 'components/visual/Day';
 import './DayGrid.scss';
 
-const DayGrid = props => (
-  <Fragment>
-    <WeekDays />
-    <Days />
-  </Fragment>
-);
+const DayGrid = props => {
+  const weekDayNames = getWeekDayNames();
+  const monthDays = getMonthDays(new Date());
+  return (
+    <Fragment>
+      <div className="weekdays-wrapper">
+        {weekDayNames.map(name => (
+          <Weekday key={name} name={name} />
+        ))}
+      </div>
+      <div className="days-wrapper">
+        {monthDays.map(({dayNumber, date}, i) => (
+          <Day dayNumber={dayNumber} date={date} key={i} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
 
 export default DayGrid;
