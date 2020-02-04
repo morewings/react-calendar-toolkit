5ea9c13eebdb40dcb5498ad56cd4425a746df824
Dima Vyshniakov <morewings@gmail.com>
2020-02-04 09:01:52 +0100

    WIP

--- src/components/DateSelector/DateSelector.js
+++ src/components/DateSelector/DateSelector.js
@@ -5,15 +5,42 @@ import './DateSelector.scss';
 const DateSelector = props => {
   const year = getFormattedDate('y', new Date());
   const month = getFormattedDate('MMMM', new Date());
+  const setYear = yearDate => {
+    console.log(yearDate);
+  };
+  const setMonth = monthDate => {
+    console.log(monthDate);
+  };
+  const incrementMonth = monthDate => {
+    console.log(`increment month;`);
+  };
+  const decrementMonth = monthDate => {
+    console.log(`decrement month;`);
+  };
+  const setPrecision = precision => {
+    console.log('setPrecision precision');
+  };
   return (
     <div className="date-selector-wrapper">
       <div className="date-selector-buttons">
-        <button type="button">{year}</button>
-        <button type="button">{month}</button>
+        <button
+          onClick={() => {
+            setPrecision('year');
+          }}
+          type="button">
+          {year}
+        </button>
+        <button onClick={setMonth} type="button">
+          {month}
+        </button>
       </div>
       <div className="date-stepper">
-        <button type="button">⟨</button>
-        <button type="button">⟩</button>
+        <button onClick={incrementMonth} type="button">
+          ⟨
+        </button>
+        <button onClick={decrementMonth} type="button">
+          ⟩
+        </button>
       </div>
     </div>
   );
