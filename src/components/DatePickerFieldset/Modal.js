45dc9bf45680f703a269df651db3b57dd5995ea7
Dima Vyshniakov <morewings@gmail.com>
2020-02-11 01:29:01 +0100

    WIP

--- src/components/DatePickerFieldset/Modal.js
+++ src/components/DatePickerFieldset/Modal.js
@@ -1,20 +1,46 @@
-import {useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 const Modal = props => {
-  const nodeRef = useRef();
+  const modalWrapperRef = useRef();
+  const modalContentRef = useRef();
   const [isNodeAttached, setIsNodeAttached] = useState(false);
+  // const handleClick = useCallback(event => {
+  //   console.log('click!');
+  //   if (!modalContentRef.current.contains(event.target)) {
+  //     console.log('outside!');
+  //   }
+  // }, [modalContentRef.current]);
   useEffect(() => {
     if (!isNodeAttached) {
-      nodeRef.current = document.createElement('div');
-      nodeRef.current.classList.add('portal-container');
-      document.body.appendChild(nodeRef.current);
+      modalWrapperRef.current = document.createElement('div');
+      modalWrapperRef.current.classList.add('modal-container');
+      document.body.appendChild(modalWrapperRef.current);
       setIsNodeAttached(true);
     }
+    return () => {
+      isNodeAttached && modalWrapperRef.current.remove();
+    };
+  }, [isNodeAttached]);
+  useEffect(() => {
+    const handleClick = event => {
+      if (!modalContentRef.current.contains(event.target)) {
+        console.log('outside!');
+      }
+    };
+    document.addEventListener('click', handleClick, false);
+    return () => {
+      document.removeEventListener('click', handleClick, false);
+    };
   }, [isNodeAttached]);
   return isNodeAttached
-    ? ReactDOM.createPortal(props.children, nodeRef.current)
+    ? ReactDOM.createPortal(
+        <div ref={modalContentRef} className="modal-content">
+          {props.children}
+        </div>,
+        modalWrapperRef.current
+      )
     : null;
 };
 
