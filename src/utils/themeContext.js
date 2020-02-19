c31b44ac8bfe791834fa378bbe904259cff39e19
Dima Vyshniakov <dmytro.vyshniakov@signavio.com>
2020-02-19 13:43:57 +0100

    add theme context

---
+++ src/utils/themeContext.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+//
+// const ThemeContext = React.createContext({});
+//
+// export const withTheme = WrappedComponent => {
+//   const Component = props => (
+//     <ThemeContext.Provider value={props.theme}>
+//       <WrappedComponent {...props} />
+//     </ThemeContext.Provider>
+//   );
+//   return Component;
+// };
+//
+// export default ThemeContext;
