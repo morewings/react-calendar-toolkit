99c89597005bd3c95eb85c6f241edb58bdf38172
Dima Vyshniakov <morewings@gmail.com>
2020-03-26 12:16:24 +0100

    add tests

---
+++ src/components/DatePicker/useLogic.spec.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {renderHook, act} from '@testing-library/react-hooks';
+// import {DatepickerMockProvider as Provider} from 'utils/testProvider';
+import createStoreProvider from 'store';
+import DatePickerContext, {
+  useDatePickerContext,
+} from 'features/datepicker/context';
+import DatepickerReducer from 'features/datepicker/DatepickerReducer';
+import useLogic from './useLogic';
+
+const mockReducer = jest.fn((...args) => {
+  DatepickerReducer(...args);
+});
+
+const onDateSet = jest.fn();
+
+const date = new Date(2020, 0, 12); // 12.12.2012
+
+const createStoreProviderWithState = state =>
+  createStoreProvider({
+    initialState: state,
+    reducer: mockReducer,
+    context: DatePickerContext,
+  });
+
+describe('DatePicker > useLogic > handleDateSet', () => {
+  beforeEach(() => {
+    onDateSet.mockClear();
+    mockReducer.mockClear();
+  });
+
+  it('returns function', () => {
+    const Provider = createStoreProviderWithState({});
+    const {result} = renderHook(() => useLogic(onDateSet, 'day'), {
+      wrapper: ({children}) => <Provider>{children}</Provider>,
+    });
+    expect(result.current.handleDateSet).toBeInstanceOf(Function);
+  });
+
+  it('sets date and calls callback when `precision === minPrecision`', () => {
+    const precision = 'day';
+    const Provider = createStoreProviderWithState({precision});
+    const {result} = renderHook(() => useLogic(onDateSet, precision), {
+      wrapper: ({children}) => <Provider>{children}</Provider>,
+    });
+    act(() => {
+      result.current.handleDateSet(date);
+    });
+    expect(onDateSet).toHaveBeenCalledTimes(1);
+    expect(onDateSet).toHaveBeenCalledWith(date);
+    expect(mockReducer).toHaveBeenCalledTimes(1);
+    expect(mockReducer.mock.calls[0][1]).toMatchSnapshot(); // SET_DATE
+  });
+
+  it('increments precision and sets visibility when `precision !== minPrecision`', () => {
+    const minPrecision = 'day';
+    const precision = 'month';
+    const Provider = createStoreProviderWithState({precision});
+    const {result} = renderHook(() => useLogic(onDateSet, minPrecision), {
+      wrapper: ({children}) => <Provider>{children}</Provider>,
+    });
+    act(() => {
+      result.current.handleDateSet(date);
+    });
+    expect(onDateSet).not.toHaveBeenCalled();
+    expect(mockReducer).toHaveBeenCalledTimes(2);
+    expect(mockReducer.mock.calls[0][1]).toMatchSnapshot(); // SET_VISIBILITY
+    expect(mockReducer.mock.calls[1][1]).toMatchSnapshot(); // SET_PRECISION
+  });
+});
