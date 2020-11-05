d93fc8e31f388faa7c0bc8ff2ba45beef7fc5eaf
Dima Vyshniakov <morewings@gmail.com>
2020-11-05 15:44:57 +0100

    improve types

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,8 @@ declare module 'react-calendar-toolkit' {
 
   type InputModes = 'popover' | 'modal';
 
-  type DefaultTheme = Record<string, string>
-  
+  type DefaultTheme = Record<string, string>;
+
   interface DatePickerDateFnsLocale {}
 
   interface DateMatcherInterface {
@@ -39,7 +39,7 @@ declare module 'react-calendar-toolkit' {
     highlightWeekends?: boolean;
   }
 
-  interface DatePickerProps extends DatePickerPropsShared{
+  interface DatePickerProps extends DatePickerPropsShared {
     onDateSet: (date: Date) => any;
     dateFnsLocale?: DatePickerDateFnsLocale;
     theme?: object;
@@ -49,35 +49,54 @@ declare module 'react-calendar-toolkit' {
     onDateSet: (date: Date) => any;
     dateFnsLocale?: DatePickerDateFnsLocale;
     theme?: object;
-    mode?: InputModes,
-    hideOnSelect?: boolean,
+    mode?: InputModes;
+    hideOnSelect?: boolean;
     renderInputAs?: React.ReactNode;
     renderDatePickerAs?: React.ReactNode;
     wrapPopoverWith?: React.ReactNode;
     wrapModalWith?: React.ReactNode;
     formatPattern?: string;
-    datePickerProps?: DatePickerPropsShared
+    datePickerProps?: DatePickerPropsShared;
   }
 
   export const DatePicker: React.FC<DatePickerProps>;
 
   export const DatePickerInput: React.FC<InputProps>;
 
-  export function useFormatDate(): (pattern: string, date: Date ) => string;
+  export function useFormatDate(): (pattern: string, date: Date) => string;
 
-  export function useThemePostCSS(defaultTheme?: DefaultTheme): [Object, Function];
+  export function useThemePostCSS(
+    defaultTheme?: DefaultTheme
+  ): [Object, Function];
 
-  export function useScrollIntoView(ref: object, containerSelector: string, condition: boolean): void;
+  export function useScrollIntoView(
+    ref: React.RefObject<HTMLInputElement>,
+    containerSelector: string,
+    condition: boolean
+  ): void;
 
-  export function useOnClickOutside(ref: object, handler: Function): void;
+  export function useOnClickOutside(
+    ref: React.RefObject<HTMLInputElement>,
+    handler: Function
+  ): void;
 
   export function useThemeContext(): object;
 
-  export function setCSSVariable(element: HTMLElement, variableName: string, value: string): void;
+  export function setCSSVariable(
+    element: HTMLElement,
+    variableName: string,
+    value: string
+  ): void;
 
-  export function getCSSVariable(element: HTMLElement, variableName: string): string;
+  export function getCSSVariable(
+    element: HTMLElement,
+    variableName: string
+  ): string;
 
-  export function removeCSSVariable(element: HTMLElement, variableName: string): void;
+  export function removeCSSVariable(
+    element: HTMLElement,
+    variableName: string
+  ): void;
 
   export const defaultTheme: DefaultTheme;
 
