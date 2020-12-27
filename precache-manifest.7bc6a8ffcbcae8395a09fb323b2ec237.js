6550f8b2039b64c2c4ee5c4af409f343fc904bbd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-27 20:38:24 +0000

    Deploy morewings/react-calendar-toolkit to github.com/morewings/react-calendar-toolkit.git:gh-pages

---
+++ precache-manifest.7bc6a8ffcbcae8395a09fb323b2ec237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf839a93fe11380d76494c7992da2db",
+    "url": "https://morewings.github.io/react-calendar-toolkit/index.html"
+  },
+  {
+    "revision": "e0400cc7bfbdefc202b6",
+    "url": "https://morewings.github.io/react-calendar-toolkit/static/css/main.26eb6bb8.chunk.css"
+  },
+  {
+    "revision": "6329866a2fe585c27071",
+    "url": "https://morewings.github.io/react-calendar-toolkit/static/js/2.9888ef66.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "https://morewings.github.io/react-calendar-toolkit/static/js/2.9888ef66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0400cc7bfbdefc202b6",
+    "url": "https://morewings.github.io/react-calendar-toolkit/static/js/main.77bc36b0.chunk.js"
+  },
+  {
+    "revision": "ff58383bd88e6751a200",
+    "url": "https://morewings.github.io/react-calendar-toolkit/static/js/runtime-main.7d5aafe5.js"
+  }
+]);
